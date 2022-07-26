5fc03c257c8ed0a48f193fe84c7fcc9de3db7388
IotaHydrae <writeforever@foxmail.com>
2022-07-27 00:02:22 +0800

    update blink
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

--- blink/main.c
+++ blink/main.c
@@ -4,5 +4,15 @@ int main()
 {
 	const uint led_pin = PICO_DEFAULT_LED_PIN;
 
+	gpio_init(led_pin);
+	gpio_set_dir(led_pin, GPIO_OUT);
+
+	while(true) {
+		gpio_put(led_pin, 1);
+		sleep_ms(50);
+		gpio_put(led_pin, 0);
+		sleep_ms(50);
+	}
+
 	return 0;
 }
