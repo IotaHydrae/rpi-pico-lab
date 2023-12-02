a22b5664b0820da028680d5b5503ba56f99fb077
iotah <writeforever@foxmail.com>
2023-12-03 01:20:05 +0800

    d51e5ta7601 is okay.
    Signed-off-by: iotah <writeforever@foxmail.com>

---
+++ D51E5TA7601/ns2009.c
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * (C) 2023 Iota Hydrae <writeforever@foxmail.com>
+ */
+
+/*
+ * Support for the ns2009
+ * Note: This is a GPIO based driver, the most obvious
+ * problem is the lag of refresh
+ */
