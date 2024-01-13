00aaaf1677a04029fd92bde191cdcad53a854a59
iotah <writeforever@foxmail.com>
2024-01-13 15:25:58 +0800

    ili9486 spi sample, not finished.
    Signed-off-by: iotah <writeforever@foxmail.com>

---
+++ ili9486-spi-lvgl/main.c
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: GPL-2.0+
+/*
+ * (C) 2023 Iota Hydrae <writeforever@foxmail.com>
+ */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "pico/platform.h"
+#include "pico/stdio.h"
+#include "pico/stdio_uart.h"
+#include "pico/stdlib.h"
+#include "hardware/pll.h"
+#include "hardware/vreg.h"
+#include "hardware/clocks.h"
+#include "hardware/timer.h"
+
+#include "lvgl/lvgl.h"
+#include "lvgl/demos/lv_demos.h"
+#include "lvgl/examples/lv_examples.h"
+#include "pico/time.h"
+#include "porting/lv_port_disp_template.h"
+
+// bool lv_tick_timer_callback(struct repeating_timer *t)
+// {
+//     lv_timer_handler();
+//     return true;
+// }
+
+#define PICO_FLASH_SPI_CLKDIV 2
+#define CPU_SPEED_MHZ 240
+
+int main(void)
+{
+    vreg_set_voltage(VREG_VOLTAGE_DEFAULT);
+    set_sys_clock_khz(CPU_SPEED_MHZ * 1000, true);
+    clock_configure(clk_peri,
+                    0,
+                    CLOCKS_CLK_PERI_CTRL_AUXSRC_VALUE_CLK_SYS,
+                    CPU_SPEED_MHZ * MHZ,
+                    CPU_SPEED_MHZ * MHZ);
+
+    stdio_uart_init_full(uart0, 115200, 16, 17);
+    printf("\n\n\nfl350hvc03v10 LVGL Porting\n");
+
+    lv_init();
+    lv_port_disp_init();
+
+    printf("Starting demo\n");
+    lv_demo_widgets();
+    // lv_demo_stress();
+    // lv_demo_music();
+    // lv_demo_benchmark();
+    
+    // struct repeating_timer timer;
+    // add_repeating_timer_ms(5, lv_tick_timer_callback, NULL, &timer);
+
+    printf("going to loop, %lld\n", time_us_64());
+    for (;;) {
+        // tight_loop_contents();
+        lv_tick_inc(5);
+        lv_timer_handler();
+        sleep_ms(5);
+    }
+
+    return 0;
+}
---
+++ ili9486-spi-lvgl/pio/pio_spi_tx.c
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/dma.h"
+#include "hardware/gpio.h"
+
+#include "pio_spi_tx.pio.h"
+
+#define USE_DMA 1
+
+#define PIN_RS  27
+#define PIN_CS  17
+
+#define PIO_CLK_DIV 6.f
+
+static PIO g_pio = pio0;
+static uint g_sm = 0;
+
+void pio_spi_tx_set_rs(bool rs)
+{
+    gpio_put_masked((1u << PIN_RS), !!rs << PIN_RS);
+}
+
+void pio_spi_tx_set_cs(bool cs)
+{
+    gpio_put_masked((1u << PIN_CS), !!cs << PIN_CS);
+}
+
+void pio_spi_tx_set_rs_cs(bool rs, bool cs) {
+    pio_spi_tx_set_rs(rs);
+    pio_spi_tx_set_cs(cs);
+}
+
+#if USE_DMA
+/* DMA version */
+static uint dma_tx;
+static dma_channel_config c;
+static inline int __time_critical_func(pio_spi_tx_write8)(PIO pio, uint sm, uint8_t *buf, size_t len)
+{
+    // const uint dma_tx = dma_claim_unused_channel(true);
+    // dma_channel_config c = dma_channel_get_default_config(dma_tx);
+
+    // channel_config_set_transfer_data_size(&c, DMA_SIZE_16);
+    // channel_config_set_dreq(&c, pio_get_dreq(pio, sm, true));
+
+    dma_channel_configure(dma_tx, &c,
+                          &pio->txf[sm], /* write address */
+                          buf, /* read address */
+                          len, /* element count (each element is of size transfer_data_size) */
+                          false /* don't start yet */
+    );
+
+    dma_start_channel_mask(1u << dma_tx);
+    dma_channel_wait_for_finish_blocking(dma_tx);
+
+    // dma_channel_unclaim(dma_tx);
+    return 0;
+}
+#else
+static inline void __time_critical_func(pio_spi_tx_write8)(PIO pio, uint sm, void *buf, size_t len)
+{
+    uint8_t data;
+
+    while (len) {
+        data = *(uint8_t *)buf;
+
+        pio_spi_tx_put(pio, sm, data);
+
+        buf++;
+        len--;
+    }
+}
+#endif
+
+int pio_spi_write_buf_rs(uint8_t *buf, size_t len, int rs)
+{
+    pio_spi_tx_wait_idle(g_pio, g_sm);
+    pio_spi_tx_set_rs(rs);
+    pio_spi_tx_wait_idle(g_pio, g_sm);
+    pio_spi_tx_write8(g_pio, g_sm, buf, len);
+    pio_spi_tx_wait_idle(g_pio, g_sm);
+    return 0;
+}
+
+int pio_spi_tx_init(void)
+{
+    printf("PIO SPI TX initialzing...\n");
+
+#if USE_DMA
+    dma_tx = dma_claim_unused_channel(true);
+    c = dma_channel_get_default_config(dma_tx);
+
+    channel_config_set_transfer_data_size(&c, DMA_SIZE_8);
+    channel_config_set_dreq(&c, pio_get_dreq(g_pio, g_sm, true));
+#endif
+
+    uint offset = pio_add_program(g_pio, &pio_spi_tx_program);
+    pio_spi_tx_program_init(g_pio, g_sm, offset, 19, 18, PIO_CLK_DIV);
+}
