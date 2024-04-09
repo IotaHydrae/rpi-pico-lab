9ba21854bcc15c0155cde8ff29aeb8e844d7134a
iotah <writeforever@foxmail.com>
2024-04-09 14:28:03 +0800

    add initial gpio based 74hc595 8080 simulation support.
    we will make a PIO version later.
    
    Signed-off-by: iotah <writeforever@foxmail.com>

--- 74hc595/main.c
+++ 74hc595/main.c
@@ -9,6 +9,15 @@
 extern void tft_init(void);
 extern void tft_test_init(void);
 
+static void gpiod_set_value(int pin, bool val)
+{
+    asm volatile("nop\n nop\n nop\n nop\n nop\n nop\n");
+    gpio_put(pin, val);
+    asm volatile("nop\n nop\n nop\n nop\n nop\n nop\n");
+}
+
+#define gpio_put gpiod_set_value
+
 void hc595_out8(uint8_t val)
 {
     gpio_put(HC595_PIN_SCK, 0);
@@ -24,35 +33,27 @@ void hc595_out8(uint8_t val)
 
 void hc595_out16(uint16_t val)
 {
-    gpio_put(HC595_PIN_SCK, 0);
+    int i;
+    val = (val << 8) | (val >> 8);
+    
     gpio_put(HC595_PIN_RCK, 0);
-
-    // for (int i = 0; i < 16; i++) {
-    //     gpio_put(HC595_PIN_DS, val & 0x8000);
-    //     gpio_put(HC595_PIN_SCK, 1);
-    //     val <<= 1;
-    //     gpio_put(HC595_PIN_SCK, 0);
-    // }
-
-    for (int i = 0; i < 8; i++) {
-        gpio_put(HC595_PIN_DS, val & 0x1);
-        gpio_put(HC595_PIN_SCK, 1);
-        val >>= 1;
+    
+    for (i = 0; i < 16; i++) {
         gpio_put(HC595_PIN_SCK, 0);
-    }
-
-    for (int i = 0; i < 8; i++) {
-        gpio_put(HC595_PIN_DS, val & 0x1);
+        gpio_put(HC595_PIN_DS, val & 0x8000);
         gpio_put(HC595_PIN_SCK, 1);
-        val >>= 1;
-        gpio_put(HC595_PIN_SCK, 0);
+        val <<= 1;
     }
 
     gpio_put(HC595_PIN_RCK, 1);
+    gpio_put(HC595_PIN_SCK, 0);
+    gpio_put(HC595_PIN_RCK, 0);
 }
 
 int main()
 {
+    set_sys_clock_48mhz();
+
     gpio_init(HC595_PIN_DS);
     gpio_init(HC595_PIN_RCK);
     gpio_init(HC595_PIN_SCK);
@@ -64,17 +65,20 @@ int main()
     gpio_set_drive_strength(HC595_PIN_DS, GPIO_DRIVE_STRENGTH_2MA);
     gpio_set_drive_strength(HC595_PIN_RCK, GPIO_DRIVE_STRENGTH_2MA);
     gpio_set_drive_strength(HC595_PIN_SCK, GPIO_DRIVE_STRENGTH_2MA);
+    
+    gpio_set_slew_rate(HC595_PIN_DS, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(HC595_PIN_RCK, GPIO_SLEW_RATE_FAST);
+    gpio_set_slew_rate(HC595_PIN_SCK, GPIO_SLEW_RATE_FAST);
 
+    // tft_test_init();
 
-    tft_test_init();
+    // uint16_t val = 0x11;
+    // // val <<= 8;
+    // for (;;) {
+    //     // hc595_out8(0x11);
+    //     hc595_out16(val);
+    // }
 
-    uint16_t val = 0x11;
-    // val <<= 8;
-    for (;;) {
-        // hc595_out8(0x11);
-        hc595_out16(val);
-    }
-
-    // tft_init();
+    tft_init();
     return 0;
 }
--- 74hc595/tft.c
+++ 74hc595/tft.c
@@ -1,4 +1,6 @@
+#include <stdarg.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
 
 #define TFT_PIN_CS  18
 #define TFT_PIN_WR  19
@@ -7,56 +9,128 @@
 #define TFT_PIN_RST 22
 #define TFT_PIN_BLK 28
 
+// struct tft_priv {
+//     uint8_t                      *buf;
+
+//     struct {
+//         int reset;
+//         int cs;   /* chip select */
+//         int rs;   /* register/data select */
+//         int wr;   /* write signal */
+//         int rd;   /* read signal */
+//         int bl;   /* backlight */
+//         int db[16];
+//     } gpio;
+    
+//     /* device specific */
+//     struct tft_display    *display;
+//     struct tft_ops        *tftops;
+// } __attribute__((__aligned__(4)));
+
+// void fbtft_write_gpio16_wr_rs(struct tft_priv *priv, void *buf, size_t len, bool rs)
+// {
+//     gpio_put(TFT)
+//     fbtft_write_gpio16_wr(priv, buf, len);
+// }
+
+// #define define_tft_write_reg(func, reg_type) \
+// void func(struct tft_priv *priv, int len, ...)  \
+// {   \
+//     reg_type *buf = (reg_type *)priv->buf; \
+//     va_list args;   \
+//     int i;  \
+//     \
+//     va_start(args, len);    \
+//     *buf = (reg_type)va_arg(args, unsigned int); \
+//     write_buf_rs(priv, buf, sizeof(reg_type), 0); \
+//     len--;  \
+//     \
+//     /* if there no privams */  \
+//     if (len == 0)  \
+//         goto exit_no_param; \
+//     \
+//     for (i = 0; i < len; i++) { \
+//         *buf++ = (reg_type)va_arg(args, unsigned int); \
+//     }   \
+//     \
+//     len *= sizeof(reg_type);    \
+//     write_buf_rs(priv, priv->buf, len, 1);  \
+// exit_no_param:  \
+//     va_end(args);   \
+// }
+
+// define_tft_write_reg(tft_write_reg16, uint16_t)
+
 extern void hc595_out16(uint16_t val);
+
+static void gpiod_set_value(int pin, bool val)
+{
+    asm volatile("nop\n nop\n nop\n nop\n nop\n nop\n");
+    gpio_put(pin, val);
+    asm volatile("nop\n nop\n nop\n nop\n nop\n nop\n");
+}
+
+#define gpio_put gpiod_set_value
 
 static void write_cmd(uint16_t val)
 {
-    val <<= 8;
+    gpio_put(TFT_PIN_CS, 0);
     gpio_put(TFT_PIN_DC, 0);
-    // busy_wait_us(1);
+
+    gpio_put(TFT_PIN_WR, 0);
     hc595_out16(val);
+    gpio_put(TFT_PIN_WR, 1);
+    // gpio_put(TFT_PIN_WR, 0);
+    // gpio_put(TFT_PIN_CS, 1);
 }
 
 static void write_data(uint16_t val)
 {
-    val <<= 8;
+    gpio_put(TFT_PIN_CS, 0);
     gpio_put(TFT_PIN_DC, 1);
-    // busy_wait_us(1);
+
+    gpio_put(TFT_PIN_WR, 0);
     hc595_out16(val);
+    gpio_put(TFT_PIN_WR, 1);
+    // gpio_put(TFT_PIN_WR, 0);
+    // gpio_put(TFT_PIN_CS, 1);
 }
 
 static void tft_reset(void)
 {
     gpio_put(TFT_PIN_RST, 1);
-    busy_wait_ms(10);
+    sleep_ms(10);
     gpio_put(TFT_PIN_RST, 0);
-    busy_wait_ms(10);
+    sleep_ms(10);
     gpio_put(TFT_PIN_RST, 1);
-    busy_wait_ms(10);
+    sleep_ms(10);
 }
 
 static void tft_init_display(void)
 {
     tft_reset();
-
     gpio_put(TFT_PIN_RD, 1);
     busy_wait_ms(120);
+    
+    // write_cmd(0xffff);
 
     write_cmd(0X11);
-    busy_wait_ms(20);
+    sleep_ms(20);
+    
+    // for (;;);
 
     write_cmd(0XD0);//VCI1  VCL  VGH  VGL DDVDH VREG1OUT power amplitude setting
-    write_data(0X07); 
-    write_data(0X42); 
-    write_data(0X1D); 
+    write_data(0X07);
+    write_data(0X42);
+    write_data(0X1D);
     write_cmd(0XD1);//VCOMH VCOM_AC amplitude setting
     write_data(0X00);
     write_data(0X1a);
-    write_data(0X09); 
+    write_data(0X09);
     write_cmd(0XD2);//Operational Amplifier Circuit Constant Current Adjust , charge pump frequency setting
     write_data(0X01);
     write_data(0X22);
-    write_cmd(0XC0);//REV SM GS 
+    write_cmd(0XC0);//REV SM GS
     write_data(0X10);
     write_data(0X3B);
     write_data(0X00);
@@ -78,14 +152,14 @@ static void tft_init_display(void)
     write_data(0X77);
     write_data(0X50);
     write_data(0X0f);
-    write_data(0X00);	  
-        
+    write_data(0X00);
+
     write_cmd(0X0D);//Get_display_mode (0Dh)
-    write_data(0X00);	
-    write_data(0X00);	
+    write_data(0X00);
+    write_data(0X00);
 
     write_cmd(0XF8);
-    write_data(0X01);	  
+    write_data(0X01);
 
     write_cmd(0XFE);
     write_data(0X00);
@@ -98,20 +172,20 @@ static void tft_init_display(void)
 
     write_cmd(0X3A);
     write_data(0X55);
-    write_cmd(0X2B);
-    write_data(0X00);
-    write_data(0X00);
-    write_data(0X01);
-    write_data(0X3F);
+    
+    // write_cmd(0X2B);
+    // write_data(0X00);
+    // write_data(0X00);
+    // write_data(0X01);
+    // write_data(0X3F);
 
-    write_cmd(0X2A);
-    write_data(0X00);
-    write_data(0X00);
-    write_data(0X01);
-    write_data(0XDF);
-    busy_wait_ms(120);
+    // write_cmd(0X2A);
+    // write_data(0X00);
+    // write_data(0X00);
+    // write_data(0X01);
+    // write_data(0XDF);
+
     write_cmd(0X29);
-
     write_cmd(0x21);
 }
 
@@ -138,7 +212,7 @@ static void tft_clear(uint16_t color)
     uint16_t height = 320;
     int x, y;
 
-    tft_set_addr_win(0, 0, width, height);
+    tft_set_addr_win(0, 0, width-1, height-1);
     for (x = 0; x < width; x++)
         for (y = 0; y < height; y++)
             write_data(color);
@@ -183,8 +257,21 @@ void tft_init(void)
     gpio_set_dir(TFT_PIN_RST, GPIO_OUT);
     gpio_set_dir(TFT_PIN_BLK, GPIO_OUT);
 
-    gpio_put(TFT_PIN_BLK, 1);
+    // origin:
+    // 0 0 1 0 1 1 0 0  0 1 0 0 1 0 0 0
+    // this:
+    // 0 1 0 0 1 0 0 0  0 0 1 0 1 1 0 0
+    // bit swab:
+    // 0 0 1 0 1 1 0 0  0 1 0 0 1 0 0 0
 
     tft_init_display();
-    tft_clear(0x1234);
+    gpio_put(TFT_PIN_BLK, 1);
+    for (;;);
+    
+    for (;;) {
+        tft_clear(0x1234);
+    }
+    // for (int color = 0; color < 0xffff; color += 128) {
+    //     tft_clear(color);
+    // }
 }
